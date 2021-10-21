91981d9aa249129c7fc9a4f3dd3becd3cc7aef2e
mahbub <mahbub21alam2017@gmail.com>
2021-10-21 14:52:49 +0600

    Components directory created

---
+++ src/components/Calculator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div className="button">
+        <button> </button>
+      </div>
+    );
+  }
+}
+export default Calculator;
