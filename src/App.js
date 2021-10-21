345beab085a1b1bcd41dbe9ee1621092d284a74b
mahbub <mahbub21alam2017@gmail.com>
2021-10-21 20:47:12 +0600

    Add design for calculator

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Calculator from './components/Calculator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="calc"><Calculator /></div>
+  );
+}
+export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import '../App.css';
 
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { result: 0 };
+  }
+
   render() {
+    const { result } = this.state;
     return (
-      <div className="button">
-        <button> </button>
+      <div>
+        <p className="result">{result}</p>
+        <div className="button">
+          <button type="button" className="grayBtn">AC</button>
+          <button type="button" className="grayBtn">+/-</button>
+          <button type="button" className="grayBtn">% </button>
+          <button type="button" className="orangeBtn">÷</button>
+          <button type="button" className="grayBtn">7</button>
+          <button type="button" className="grayBtn">8</button>
+          <button type="button" className="grayBtn">9</button>
+          <button type="button" className="orangeBtn">x</button>
+          <button type="button" className="grayBtn">4</button>
+          <button type="button" className="grayBtn">5</button>
+          <button type="button" className="grayBtn">6</button>
+          <button type="button" className="orangeBtn">-</button>
+          <button type="button" className="grayBtn">1</button>
+          <button type="button" className="grayBtn">2</button>
+          <button type="button" className="grayBtn">3</button>
+          <button type="button" className="orangeBtn">+</button>
+          <button type="button" className="grayBtn" id="zero">0</button>
+          <button type="button" className="grayBtn">.</button>
+          <button type="button" className="orangeBtn">=</button>
+        </div>
       </div>
     );
   }
