a5ea72d99d853fa1f31fbada8ae5e10fd6c34754
mahbub <mahbub21alam2017@gmail.com>
2021-10-22 23:42:55 +0600

    Refactor  from a class based component to a functional component with hooks.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,55 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
 
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-
-    this.handleEvent = this.handleEvent.bind(this);
-  }
-
-  handleEvent = (buttonName) => {
-    this.setState((state) => calculate(state, buttonName));
+  const handleEvent = (e) => {
+    setState({ ...state, ...calculate(state, e.target.name) });
   };
 
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <div>
-        <p className="result">
-          <span>{total}</span>
-          <span>{operation}</span>
-          <span>{next}</span>
-        </p>
-        <div className="button">
-          <button type="button" className="grayBtn" name="AC" onClick={(e) => this.handleEvent(e.target.name)}>AC</button>
-          <button type="button" className="grayBtn" name="+/-" onClick={(e) => this.handleEvent(e.target.name)}>+/-</button>
-          <button type="button" className="grayBtn" name="%" onClick={(e) => this.handleEvent(e.target.name)}>%</button>
-          <button type="button" className="orangeBtn" name="÷" onClick={(e) => this.handleEvent(e.target.name)}>÷</button>
-          <button type="button" className="grayBtn" name="7" onClick={(e) => this.handleEvent(e.target.name)}>7</button>
-          <button type="button" className="grayBtn" name="8" onClick={(e) => this.handleEvent(e.target.name)}>8</button>
-          <button type="button" className="grayBtn" name="9" onClick={(e) => this.handleEvent(e.target.name)}>9</button>
-          <button type="button" className="orangeBtn" name="x" onClick={(e) => this.handleEvent(e.target.name)}>x</button>
-          <button type="button" className="grayBtn" name="4" onClick={(e) => this.handleEvent(e.target.name)}>4</button>
-          <button type="button" className="grayBtn" name="5" onClick={(e) => this.handleEvent(e.target.name)}>5</button>
-          <button type="button" className="grayBtn" name="6" onClick={(e) => this.handleEvent(e.target.name)}>6</button>
-          <button type="button" className="orangeBtn" name="-" onClick={(e) => this.handleEvent(e.target.name)}>-</button>
-          <button type="button" className="grayBtn" name="1" onClick={(e) => this.handleEvent(e.target.name)}>1</button>
-          <button type="button" className="grayBtn" name="2" onClick={(e) => this.handleEvent(e.target.name)}>2</button>
-          <button type="button" className="grayBtn" name="3" onClick={(e) => this.handleEvent(e.target.name)}>3</button>
-          <button type="button" className="orangeBtn" name="+" onClick={(e) => this.handleEvent(e.target.name)}>+</button>
-          <button type="button" className="grayBtn" id="zero" name="0" onClick={(e) => this.handleEvent(e.target.name)}>0</button>
-          <button type="button" className="grayBtn" name="." onClick={(e) => this.handleEvent(e.target.name)}>.</button>
-          <button type="button" className="orangeBtn" name="=" onClick={(e) => this.handleEvent(e.target.name)}>=</button>
-        </div>
+  return (
+    <div>
+      <p className="result">
+        <span>{state.total}</span>
+        <span>{state.operation}</span>
+        <span>{state.next}</span>
+      </p>
+      <div className="button">
+        <button type="button" className="grayBtn" name="AC" onClick={handleEvent}>AC</button>
+        <button type="button" className="grayBtn" name="+/-" onClick={handleEvent}>+/-</button>
+        <button type="button" className="grayBtn" name="%" onClick={handleEvent}>%</button>
+        <button type="button" className="orangeBtn" name="÷" onClick={handleEvent}>÷</button>
+        <button type="button" className="grayBtn" name="7" onClick={handleEvent}>7</button>
+        <button type="button" className="grayBtn" name="8" onClick={handleEvent}>8</button>
+        <button type="button" className="grayBtn" name="9" onClick={handleEvent}>9</button>
+        <button type="button" className="orangeBtn" name="x" onClick={handleEvent}>x</button>
+        <button type="button" className="grayBtn" name="4" onClick={handleEvent}>4</button>
+        <button type="button" className="grayBtn" name="5" onClick={handleEvent}>5</button>
+        <button type="button" className="grayBtn" name="6" onClick={handleEvent}>6</button>
+        <button type="button" className="orangeBtn" name="-" onClick={handleEvent}>-</button>
+        <button type="button" className="grayBtn" name="1" onClick={handleEvent}>1</button>
+        <button type="button" className="grayBtn" name="2" onClick={handleEvent}>2</button>
+        <button type="button" className="grayBtn" name="3" onClick={handleEvent}>3</button>
+        <button type="button" className="orangeBtn" name="+" onClick={handleEvent}>+</button>
+        <button type="button" className="grayBtn" id="zero" name="0" onClick={handleEvent}>0</button>
+        <button type="button" className="grayBtn" name="." onClick={handleEvent}>.</button>
+        <button type="button" className="orangeBtn" name="=" onClick={handleEvent}>=</button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 export default Calculator;
