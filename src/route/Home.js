848828a3fa957102401c19c4117a3c50666a9766
mahbub <mahbub21alam2017@gmail.com>
2021-10-27 16:46:04 +0600

    Add Home page things

---
+++ src/route/Home.js
@@ -0,0 +1,25 @@
+/* eslint-disable max-len */
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+const Home = () => (
+  <Route path="/">
+    <div>
+      <h1>Welcomme to our page!</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, fugit odit molestiae praesepossimus. Veniam, architecto. Labore doloremque illo minus odit nulla nesciunt eius omnis, adipisci, possimus quosexercitationem.
+        Accusamus praesentium, qui minus quam ipsa, illo reprehenderit maxime deserunt aut, iste cupiditate est repellendus natus doloremque sapiente accusantium obcaecati fugiat facere suscipit? Dolores ducimus quis, laborum esse assumenda autem.
+        Enim aut, ullam, ex maxime commodi minima nemo ea, consequatur dignissimos dolore quo necessitatibus officiis voluptatum voluptates cum! Iste dolorum obcaecati ipsam incidunt distinctio debitis a, atque consectetur dolores amet!
+        Molestiae impedit cupiditate nam non quibusdam, harum eligendi doloribus magnam ea.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, fugit odit molestiae praesepossimus. Veniam, architecto. Labore doloremque illo minus odit nulla nesciunt eius omnis, adipisci, possimus quosexercitationem.
+        Accusamus praesentium, qui minus quam ipsa, illo reprehenderit maxime deserunt aut, iste cupiditate est repellendus natus doloremque sapiente accusantium obcaecati fugiat facere suscipit? Dolores ducimus quis, laborum esse assumenda autem.
+        Enim aut, ullam, ex maxime commodi minima nemo ea, consequatur dignissimos dolore quo necessitatibus officiis voluptatum voluptates cum! Iste dolorum obcaecati ipsam incidunt distinctio debitis a, atque consectetur dolores amet!
+        Molestiae impedit cupiditate nam non quibusdam, harum eligendi doloribus magnam ea.
+      </p>
+    </div>
+  </Route>
+);
+
+export default Home;
