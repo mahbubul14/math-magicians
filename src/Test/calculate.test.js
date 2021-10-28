21861794c011e4d7bb89c221d2b8b1e3dceb4207
Bishoy <bishoy.samwuel@gmail.com>
2021-10-28 14:08:38 +0200

    Add tests for Division and Modulus

--- src/Test/calculate.test.js
+++ src/Test/calculate.test.js
@@ -1,6 +1,7 @@
 import calculate from '../logic/calculate';
 
 describe('testing calculate function', () => {
+  const obj = {};
   test('undefined when calculate by zero', () => {
     const obj = {
       total: '0',
@@ -19,5 +20,18 @@ describe('testing calculate function', () => {
     };
     const buttonName = '0';
     expect(calculate(obj, buttonName).total).toBe(0);
+  });
+
+  test('Division', () => {
+    obj.operation = '÷';
+    obj.total = 6;
+    obj.next = 3;
+    expect(calculate(obj, '=')).toEqual({ next: null, operation: null, total: '2' });
+  });
+  test('Modulus', () => {
+    obj.operation = '%';
+    obj.total = 8;
+    obj.next = 2;
+    expect(calculate(obj, '=')).toEqual({ next: null, operation: null, total: '0' });
   });
 });
