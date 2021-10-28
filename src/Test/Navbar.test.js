66f43f54043d5aa270b4e4a66824a09ee62fd983
Bishoy <bishoy.samwuel@gmail.com>
2021-10-28 16:59:01 +0200

    Add Navbar test

---
+++ src/Test/Navbar.test.js
@@ -0,0 +1,16 @@
+// import { React } from 'react';
+// import { Router } from 'react-router';
+// import renderer from 'react-test-renderer';
+
+// import Navbar from '../components/Navbar';
+
+it('renders', () => {
+  // const tree = renderer
+  //   .create(
+  //     <Router>
+  //       <Navbar />
+  //     </Router>
+  //     ,
+  //   ).toJSON();
+  // expect(tree).toMatchSnapshot();
+});
